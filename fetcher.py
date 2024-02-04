925252c924343357db00d25399f646cd92cd97f8
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-02-04 11:49:36 -0500

    Created fetcher.py to fetch stock details

---
+++ fetcher.py
@@ -0,0 +1,7 @@
+import yfinance as yf
+import pandas as pd
+
+def get_stock_data(ticker, start_date, end_date):
+    stock_data = yf.download(ticker, start=start_date, end=end_date)
+    stock_data.reset_index(inplace=True)
+    return stock_data
